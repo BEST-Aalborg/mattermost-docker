b7e3ede2c5f44634da087267c73e06cba1ab843b
Dennis Værum <github@varum.dk>
2019-03-14 22:24:19 +0100

    updated and improved the update script

--- update_2_latest_version.sh
+++ update_2_latest_version.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+_PWD="$(dirname "$(readlink -f "$0")")"
+
 get_latest_release() {
-  curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
+  curl --silent "https://api.github.com/repos/$1/releases" | # Get latest release from GitHub api
     grep '"tag_name":' |                                            # Get tag line
-    sed -E 's/.*"v([^"]+)".*/\1/'                                    # Pluck JSON value
+    sed -E 's/.*"v([^"]+)".*/\1/' |                                 # Pluck JSON value
+    sort -r |                                                       # Sort the version with the highest first
+    head -n 1                                                       # Pick the top version
 }
 
 release=$(get_latest_release "mattermost/mattermost-server")
 
-sed -i -E "s/^(ENV MM_VERSION).+/\1=${release}/" "$HOME/mattermost-docker/app/Dockerfile"
+sed -i -E "s/^(ENV MM_VERSION).+/\1=${release}/" "$_PWD/app/Dockerfile"
 
+for name in $(find $_PWD -iname Dockerfile -exec sed -nr 's/FROM[[:space:]]+//p' '{}' \;); do
+    docker pull $name
+done
